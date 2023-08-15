c6c3d66564b2a9eb854acd807ae48ffbad08bd73
Mano <mano.beeslaar@gmail.com>
2023-08-15 21:23:21 +0200

    initial commit

---
+++ job_board/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+#job posting table
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salary = models.IntegerField()
